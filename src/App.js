887d8d796e0d3fed9500b2c324e02e32c175407d
teagandong <teagandong@gmail.com>
2024-06-14 13:26:38 +0800

    added the GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Teagan Heath and is{" "}
+        <a
+          href="https://github.com/teagandong/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
