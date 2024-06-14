c63385f6ba0bcafa4c176e2a89a3de71934dfd70
teagandong <teagandong@gmail.com>
2024-06-14 13:46:24 +0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by Teagan Heath and is{" "}
-        <a
-          href="https://github.com/teagandong/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by Teagan Heath and is{" "}
+          <a
+            href="https://github.com/teagandong/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
